29fd139949237b2a74190f7130f46ce26d9a67f5
Vikram Singh <vikramhansawat@yahoo.com>
2022-08-10 13:10:09 +0530

    [FIX] type for println

--- libraries/splunk/steps/splunk_step_watcher.groovy
+++ libraries/splunk/steps/splunk_step_watcher.groovy
@@ -1,9 +1,9 @@
 @BeforeStep
 void before(){
-    pringln "Splunk: running before the ${hookContext.library} library's ${hookContext.step} step"
+    println "Splunk: running before the ${hookContext.library} library's ${hookContext.step} step"
 }
 
 @AfterStep
 void after(){
-    pringln "Splunk: running after the ${hookContext.library} library's ${hookContext.step} step"
+    println "Splunk: running after the ${hookContext.library} library's ${hookContext.step} step"
 }
